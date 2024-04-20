a051cbb7d398f2ab8418490f960262c9987042a4
Hendrik-Jan <h@vdwaal.net>
2024-04-20 10:23:51 +0200

    first commit

---
+++ import.py
@@ -0,0 +1,35 @@
+"""Script to port kunena 5 to bbpress"""
+
+import datetime
+from dateutil import tz
+from mysql.connector import connect, Error
+
+##Some setup needs to come here
+
+try:
+    connection = connect(
+        host="87.236.98.81", password="2548tk", user="picknl", database="picknl"
+    )
+    print(connection)
+except Error as e:
+    print(e)
+    exit()
+
+
+SELECT_QUERY = """
+    SELECT id, subject, first_post_id, first_post_time
+    FROM nft_kunena_topics
+    where category_id=11
+    """
+with connection.cursor() as cursor:
+    cursor.execute(SELECT_QUERY)
+    for topic in cursor.fetchall():
+        print(topic)
+        print(topic[3])
+        date = datetime.datetime.fromtimestamp(topic[3])
+        print(date)
+        date = date.replace(tzinfo=tz.tzlocal())
+        utc_time = date.astimezone(tz.tzutc())
+        print(utc_time)
+
+connection.close()
